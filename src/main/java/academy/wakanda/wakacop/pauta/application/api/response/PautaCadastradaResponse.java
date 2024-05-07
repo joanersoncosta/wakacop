29894a440e46b6fb5c0921c85bcf8d9565d17e72
Joanerson Costa <joanersonc31@gmail.com>
2024-05-07 16:33:49 -0300

    adiciona responseStatus no API e toString no response e request

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/response/PautaCadastradaResponse.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/response/PautaCadastradaResponse.java
@@ -3,8 +3,10 @@ package academy.wakanda.wakacop.pauta.application.api.response;
 import java.util.UUID;
 
 import lombok.Builder;
+import lombok.ToString;
 
 @Builder
+@ToString
 public class PautaCadastradaResponse {
 	private UUID idPauta;
 }
