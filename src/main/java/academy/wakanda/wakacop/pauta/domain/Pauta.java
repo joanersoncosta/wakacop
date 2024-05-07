8ce764a2f75da4c9e3114735ec3eb23ddb4042db
Joanerson Costa <joanersonc31@gmail.com>
2024-05-07 15:52:25 -0300

    implementa domain pauta

---
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -0,0 +1,28 @@
+package academy.wakanda.wakacop.pauta.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Entity
+@ToString
+public class Pauta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+	private UUID idPauta;
+	private String titulo;
+	private String descricao;
+	private UUID idAssociado;
+	private LocalDateTime dataCriacao;
+}
