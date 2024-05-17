58ba73a761767f056758df06eb0d07d70eafb7dc
Joanerson Costa <joanersonc31@gmail.com>
2024-05-17 15:02:14 -0300

    git config sns and implements publisher

---
+++ src/main/java/academy/wakanda/wakacop/config/AwsConfigProperties.java
@@ -0,0 +1,25 @@
+package academy.wakanda.wakacop.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Configuration
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "aws.config")
+@ToString
+public class AwsConfigProperties {
+    private String region;
+    private String accesskey;
+    private String secretkey;
+    private String endpointuri;
+    private String resultadoSessaoTopic;
+}
---
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/infra/PublicadorResultadoSessaoInfraSNS.java
@@ -0,0 +1,26 @@
+package academy.wakanda.wakacop.sessaovotacao.infra;
+
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import academy.wakanda.wakacop.config.AwsConfigProperties;
+import academy.wakanda.wakacop.sessaovotacao.application.api.response.ResultadoSessaoVotacao;
+import academy.wakanda.wakacop.sessaovotacao.domin.PublicaResultadoResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class PublicadorResultadoSessaoInfraSNS implements PublicaResultadoResponse {
+	private final NotificationMessagingTemplate publicadorResultadoSNS;
+    private final AwsConfigProperties awsSnsProperties;
+
+	@Override
+	public void publica(ResultadoSessaoVotacao resultadoSessaoVotacao) {
+		log.debug("[start] PublicadorResultadoSessaoInfraSNS - publica");
+		publicadorResultadoSNS.sendNotification(awsSnsProperties.getResultadoSessaoTopic(), resultadoSessaoVotacao, resultadoSessaoVotacao.getIdPauta().toString());
+		log.debug("[finish] PublicadorResultadoSessaoInfraSNS - publica");
+	}
+
+}
